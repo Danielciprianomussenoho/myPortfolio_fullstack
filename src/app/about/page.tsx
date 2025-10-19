ad817150199d60085f531a033fb2d79f04a32cb4
Daniel cipriano mussenoho <Danielciprianomussenoho@gmail.com>
2025-10-19 05:54:45 +0100

    routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+ import AboutPage from "@/components/public/AboutSection";
+
+export default function About() {
+  return <AboutPage />;
+}
---
+++ src/app/skills/page.tsx
@@ -0,0 +1,5 @@
+import SkillsPage from "@/components/public/SkillsSection";
+
+export default function Skills() {
+  return <SkillsPage />;
+}
